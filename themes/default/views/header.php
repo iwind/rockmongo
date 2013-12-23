b15db125964b4dfafb529708320c5a69bc815968
vingrad <vova@vingrad.com>
2013-12-23 09:17:48 +0100

    Update header.php
    Search objects with ID (helper for GUI)

--- themes/default/views/header.php
+++ themes/default/views/header.php
@@ -12,6 +12,31 @@ $(function () {
 	$(document).click(window.parent.hideMenus);
 	if ($("textarea").length > 0) {
 		$("textarea").tabby();
+		
+		
+		// Search with ID field 
+ 		var elTextField = $("<input />").attr({type: 'text', size: 30}).keypress(function(e) {
+	            if (e.keyCode == '13') {
+	                e.preventDefault();
+	                $(this).parent().find('input:last').click();
+	            }
+	        });
+        	var elButton    = $("<input />").attr({type: 'button', value: 'Select by ID'})
+	                                        .bind('click', function (e) {
+	                                            var v = $(this).parent().find('input:first').val();
+	                                            // заветная строка!
+	                                            $('#query_form textarea').val('{\n\t\"_id\": ObjectId(\"'+v+'\")\n}');
+	                                            $('#query_form').submit();
+	                                        });
+	        $("<div />")
+	            .html('_id: ')
+	            .css({paddingLeft: 5, paddingTop: 4})
+	            .append(elTextField)
+	            .append(' ')
+	            .append(elButton)
+	            .prependTo('#query_form');
+            
+            
 	}
 });
 </script>
