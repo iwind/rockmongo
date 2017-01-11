dd68ac095a01f9624c57cd58fa549f76a52b6f97
sergey <sergey@rusoft.ru>
2017-01-11 13:05:51 +0300

    Fix label for checkbox

--- themes/default/views/db/dbImport.php
+++ themes/default/views/db/dbImport.php
@@ -17,7 +17,7 @@ JS File: <input type="file" style="width:400px" name="json"/><br/>
 Support gzipped (.js.gz) files.<br/><br/>
 
 <input type="hidden" name="split_js" value="0"/>
-<label>Do split JS File by chunks:</label> <input type="checkbox" name="split_js" value="1"/><br/>
+<label for="import_js_split">Do split JS File by chunks:</label> <input id="import_js_split" type="checkbox" name="split_js" value="1"/><br/>
 <label>Max documents in chunk:</label> <input type="numeric" name="split_max" value="1000"/><br/>
 Useful if file more than 4mb (2.x mongo) or 16mb (3.x mongo).<br/>
 
